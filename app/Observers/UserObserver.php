a6e95e3bb5ef6ab8fb35718b51d57d30c0f5b5c1
OsqY <oscarjosehpc@gmail.com>
2025-06-30 20:20:14 -0600

    added observer for user registration

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        $role = Role::where('name', 'usuario')->first();
+        if ($role) {
+            $user->assignRole($role);
+        }
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
@@ -23,9 +25,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
 
+        User::observe(UserObserver::class);
+
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
-                ->locales(['en','es']); // also accepts a closure
+                ->locales(['en', 'es']); // also accepts a closure
         });
     }
 }
